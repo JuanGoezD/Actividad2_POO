2b42c7e33bce0d9ecee3f2fccac9002d50e2642b
vmduque <124936868+vmduque@users.noreply.github.com>
2023-03-18 18:27:20 -0500

    Add files via upload

---
+++ Tarea23/src/tarea23/Tarea23.java
@@ -0,0 +1,31 @@
+package tarea23;
+import java.util.Scanner;
+
+public class Tarea23 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double a,b,c,solucion1,solucion2,discriminante;
+        
+        System.out.println("Ingrese un valor de a:");
+        a = entrada.nextDouble();
+        System.out.println("Ingrese un valor de b:");
+        b = entrada.nextDouble();
+        System.out.println("Ingrese un valor de c:");
+        c = entrada.nextDouble();
+        
+        discriminante = Math.pow(b,2) - 4 * a * c;
+        
+        if (discriminante >= 0)  {
+        solucion1 = (-b + Math.sqrt(discriminante)) / (2 * a);
+        solucion2 = (-b - Math.sqrt(discriminante)) / (2 * a);
+        
+        System.out.println("Las soluciones reales de la ecuacion son x1 = "+solucion1+" y x2 = "+solucion2);
+            }
+        else  {
+            System.out.println("La ecuacion no tiene soluciones reales");
+        }
+        
+    }
+    
+}
