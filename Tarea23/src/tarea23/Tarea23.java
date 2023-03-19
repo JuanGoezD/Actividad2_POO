b9d2bc4c107363492b9772d615eb991a092bdd36
vmduque <124936868+vmduque@users.noreply.github.com>
2023-03-18 19:17:50 -0500

    Update Tarea23.java

--- Tarea23/src/tarea23/Tarea23.java
+++ Tarea23/src/tarea23/Tarea23.java
@@ -16,12 +16,15 @@ public class Tarea23 {
         
         discriminante = Math.pow(b,2) - 4 * a * c;
         
-        if (discriminante >= 0)  {
+        if (discriminante > 0)  {
         solucion1 = (-b + Math.sqrt(discriminante)) / (2 * a);
         solucion2 = (-b - Math.sqrt(discriminante)) / (2 * a);
         
         System.out.println("Las soluciones reales de la ecuacion son x1 = "+solucion1+" y x2 = "+solucion2);
             }
+        else if (discriminante == 0)  {
+            double solucion = -b / (2 * a);
+            System.out.println("La solucion de la ecuacion es x = "+solucion);
         else  {
             System.out.println("La ecuacion no tiene soluciones reales");
         }
